e36bceedb7b5c0331d3f3d3de60d61c349a4639c
Priit Kallas <kallaspriit@gmail.com>
2015-08-05 17:39:08 +0300

    Added basic React integration.

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+import React from 'react';
+
 import log from './services/Log.js';
 import Calculator from './services/Calculator.js';
+import RootView from './views/RootView.jsx';
 
 let calculator = new Calculator();
 
 log('hello world, 4 + 2 = ' + calculator.sum(4, 2));
+
+React.render(
+    React.createElement(RootView),
+    document.getElementById('app')
+);
